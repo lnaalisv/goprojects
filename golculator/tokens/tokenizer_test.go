d12f78fcd510e533ec23345aec0655290fc3ffbe
Lauri Naalisvaara <l_naalisvaara@hotmail.com>
2021-12-24 15:48:04 +0200

    fix golculator test

--- golculator/tokens/tokenizer_test.go
+++ golculator/tokens/tokenizer_test.go
@@ -2,9 +2,24 @@ package tokens
 
 import "testing"
 
+func areEqual(a []Token, b []Token) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, _ := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
 func TestTokenize(t *testing.T) {
 	result, _ := Tokenize("+0123-500()")
-	if result != nil {
-		t.Error("Expected foo received", result)
+	expected := []Token{
+		Plus{}, Number{123}, Minus{}, Number{500}, LeftParenthesis{}, RightParenthesis{},
+	}
+	if !areEqual(result, expected) {
+		t.Error("Expected", expected, "Got", result)
 	}
 }
